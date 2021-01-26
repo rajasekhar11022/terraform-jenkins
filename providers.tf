c4ff19fe81b3334e1d29c8dc40937d437ef4d925
rajasekhar <rajasekhar11022@gmail.com>
2021-01-26 20:53:31 +0530

    thirteen Commit

--- providers.tf
+++ providers.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "terraformraviraja"
+    bucket = "terraformraviraja-1212"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
